2b8495116806ead804e3dd89d7bd3fa5ea7279e2
Fabo2303 <fabian666777@gmail.com>
2024-06-17 13:20:33 -0500

    add backend

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Course } from '../data/courses';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private http = inject(HttpClient);
+
+  getCourses() {
+    return this.http.get('http://localhost:8080/api/courses/findAll');
+  }
+}
--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@angular/core';
-import courses from '../assets/courses.json';
+import { inject, Injectable } from '@angular/core';
 import { Course } from '../data/courses';
+import { CoursesService } from './courses.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
 
-  private coursesTemp = courses as Course[];
+  private coursesService = inject(CoursesService);
+  private courses: Course[] = [];
+  private coursesTemp: Course[] = [];
+
+
+  constructor() {
+    this.coursesService.getCourses().subscribe((data) => {
+      this.courses = data as Course[];
+      this.coursesTemp = data as Course[];
+    });
+  }
 
   getActiveCourses(cursos: Course[] = this.coursesTemp) {
     return cursos.filter(course => course.status);
@@ -19,7 +29,7 @@ export class FilterService {
 
   filterCourses(searchTerm: string){
     if (!searchTerm || searchTerm.length === 0) {
-      this.coursesTemp = courses;
+      this.coursesTemp = this.courses;
     }
     this.coursesTemp = this.coursesTemp.filter(course => {
       return course.title.toLowerCase().includes(searchTerm.toLowerCase());
